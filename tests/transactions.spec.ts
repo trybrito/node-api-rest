df974cf650717ee2edd4bdd74e095ea2298d83fa
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-03-26 23:19:10 -0300

    refactor: improves test context

--- tests/transactions.spec.ts
+++ tests/transactions.spec.ts
@@ -2,16 +2,16 @@ import { describe, expect, it, beforeAll, afterAll } from 'vitest'
 import { app } from '../src/app.ts'
 import request from 'supertest'
 
-beforeAll(async () => {
-	await app.ready()
-})
+describe('Transactions routes', () => {
+	beforeAll(async () => {
+		await app.ready()
+	})
 
-afterAll(async () => {
-	await app.close()
-})
+	afterAll(async () => {
+		await app.close()
+	})
 
-describe('Transactions', () => {
-	it('should create a new transaction', async () => {
+	it('should be able to create a new transaction', async () => {
 		const response = await request(app.server).post('/transactions').send({
 			title: 'New Transaction',
 			amount: 5000,
