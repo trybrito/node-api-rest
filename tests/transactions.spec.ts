4c283a4556937bc9f45172458a6098ffe963e32e
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-03-26 23:35:51 -0300

    test: list transactions

--- tests/transactions.spec.ts
+++ tests/transactions.spec.ts
@@ -20,4 +20,29 @@ describe('Transactions routes', () => {
 
 		expect(response.status).toEqual(201)
 	})
+
+	it('should be able to list all transactions', async () => {
+		const createTransactionResponse = await request(app.server)
+			.post('/transactions')
+			.send({
+				title: 'New Transaction',
+				amount: 5000,
+				type: 'credit',
+			})
+
+		const cookies = createTransactionResponse.get('Set-Cookie') ?? []
+
+		const listTransactionResponse = await request(app.server)
+			.get('/transactions')
+			.set('Cookie', cookies)
+			.expect(200)
+
+		expect(listTransactionResponse.body.transactions.length).toBeGreaterThan(0)
+		expect(listTransactionResponse.body.transactions).toEqual([
+			expect.objectContaining({
+				title: 'New Transaction',
+				amount: 5000,
+			}),
+		])
+	})
 })
